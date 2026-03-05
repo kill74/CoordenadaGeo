13e552ec42c884f2d9a2ed28f5506e555a90e703
Guilherme Sales <gnailpo895@gmail.com>
2026-03-05 12:30:20 +0000

    Add overloaded constructors and validation checks to Elevador class

--- src/elevador/Elevador.java
+++ src/elevador/Elevador.java
@@ -7,13 +7,50 @@ public class Elevador {
 
     //Construtor
     public Elevador(int andaratual, int andarinferior, int andarsuperior) {
-        if(andarinferior >= andarsuperior){
+        if(andarinferior >= andarsuperior)
             throw new IllegalArgumentException("Andar inferior superior ao andar inferior");
-        }
+        if(andaratual < andarinferior || andaratual > andarsuperior)
+            throw new IllegalArgumentException("Andar atual invalido");
         this.andaratual = andaratual;
         this.andarinferior = andarinferior;
         this.andarsuperior = andarsuperior;
     }
+
+    public Elevador(int andarinferior, int andarsuperior) {
+        this(andarinferior, andarinferior, andarsuperior);
+    }
+
+    public Elevador(int andarsuperior) {
+        this(0,0,andarsuperior);
+    }
+
+    public boolean estaTopo(){
+        return andaratual == andarsuperior;
+    }
+
+    public boolean estaFundo(){
+        return andaratual == andarinferior;
+    }
+
+    public boolean acede(int andar){
+        return (andar >= andarinferior && andar <= andarsuperior);
+    }
+
+    public void sobe(){
+        if(estaTopo())
+            throw new  IllegalArgumentException();
+        andaratual++;
+    }
+
+    public void desce(){
+        if(estaTopo())
+            throw new  IllegalArgumentException();
+        andaratual--;
+    }
+
+
+
+
 
     //ToString
     @Override
@@ -37,16 +74,23 @@ public class Elevador {
     }
 
     public void setAndaratual(int andaratual){
+        if(andaratual < andarinferior || andaratual > andarsuperior ){
+            throw new IllegalArgumentException("Andar atual invalido");
+        }
         this.andaratual = andaratual;
     }
     public void setAndarinferior(int andarinferior){
+        if(andarinferior > andaratual || andarinferior > andarsuperior){
+            throw new IllegalArgumentException("Andar inferior invalido");
+        }
         this.andarinferior = andarinferior;
     }
     public void setAndarsuperior(int andarsuperior){
+        if(andarsuperior < andaratual || andarsuperior < andarinferior){
+            throw new IllegalArgumentException("Andar superior invalido");
+        }
         this.andarsuperior = andarsuperior;
     }
-
-
     public static void main(String[] args) {
 
     }
