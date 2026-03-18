d854cbd6df0d4c699424c00a3b202096f96a64f3
Guilherme Sales <gnailpo895@gmail.com>
2026-03-18 11:25:11 +0000

    Implement Hora class with validation checks and toString method; add TestaHora for testing

--- src/Teste/TestaHora.java
+++ src/Teste/TestaHora.java
@@ -1,4 +1,9 @@
 package Teste;
+import tempo.Hora;
 
 public class TestaHora {
+    public static void main(String[] args) {
+        Hora h1 = new Hora(23,20,300);
+        System.out.println(h1);
+    }
 }
--- src/tempo/Hora.java
+++ src/tempo/Hora.java
@@ -1,4 +1,61 @@
 package tempo;
 
 public class Hora {
+    private int hora;
+    private int min;
+    private int seg;
+
+    public Hora(int hora, int min, int seg) {
+        if (hora < 0 || hora >= 24) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        if (min < 0 || min >= 60) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        if (seg < 0 || seg >= 60) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        this.hora = hora;
+        this.min = min;
+        this.seg = seg;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        if (hora < 0 || hora >= 24) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        this.hora = hora;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        if (min < 0 || min >= 60) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        this.min = min;
+    }
+
+    public int getSeg() {
+        return seg;
+    }
+
+    public void setSeg(int seg) {
+        if (seg < 0 || seg >= 60) {
+            throw new IllegalArgumentException("Valores inválidos para hora, minuto ou segundo");
+        }
+        this.seg = seg;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d" + hora, min, seg);
+    }
+
 }
